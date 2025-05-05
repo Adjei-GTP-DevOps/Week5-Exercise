4806c34d1add80bad9a4934ac37c1bf437fa4912
Adjei <samensah036@st.ug.edu.gh>
2025-05-05 12:48:38 +0000

    setup & run queries

--- averageOrderByCountry.sql
+++ averageOrderByCountry.sql
@@ -1,15 +1,14 @@
 SELECT 
-    c.country,
+    country,
     AVG(order_total) AS avg_order_value
 FROM (
     SELECT 
         o.order_id, 
-        c.customer_id, 
-        c.country,
+        cu.country,
         SUM(oi.quantity * oi.unit_price) AS order_total
     FROM orders o
-    JOIN customers c ON o.customer_id = c.customer_id
+    JOIN customers cu ON o.customer_id = cu.customer_id
     JOIN order_items oi ON o.order_id = oi.order_id
-    GROUP BY o.order_id
+    GROUP BY o.order_id, cu.country
 ) AS order_values
 GROUP BY country;
